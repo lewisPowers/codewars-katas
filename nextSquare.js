1e1cd4ec1701b1a57dabd3456c3d93fe7ceb078b
Lewis Powers <lpowers1985@gmail.com>
2022-05-27 23:06:56 -0700

    if num is a perfect square return next

---
+++ nextSquare.js
@@ -0,0 +1,7 @@
+function findNextSquare(sq) {
+  if ( Math.sqrt(sq) !== Math.trunc(Math.sqrt(sq)) ) return -1;
+  return Math.pow(Math.sqrt(sq) + 1, 2)
+}
+
+
+console.log('Find the Next Square: ', findNextSquare(121), findNextSquare(122), findNextSquare(319225), findNextSquare(625), findNextSquare(15241383936))
