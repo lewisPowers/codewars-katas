967e97d95a2d3de30d9a768fce576069aa17ebc4
Lewis Powers <lpowers1985@gmail.com>
2022-06-11 22:16:11 -0700

    sort array and return first word with stars between letters

---
+++ sortAndStar.js
@@ -0,0 +1,10 @@
+function twoSort(arrOfStrings) {
+  let sorted = arrOfStrings.sort( (a,b) => {
+    return a < b ? -1 : a > b ? 1 : 0;
+  })
+  return sorted[0].split('').map( (letter, i, arr) => {
+    return i === arr.length - 1 ? letter : `${letter}***`;
+  }).join('');
+}
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
