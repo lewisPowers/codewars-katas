ebb975fc7b5343b37a2de43fdd160da8960f2413
Lewis Powers <lpowers1985@gmail.com>
2022-06-12 18:01:04 -0700

    commented tests

--- sortAndStar.js
+++ sortAndStar.js
@@ -6,5 +6,5 @@ function twoSort(arrOfStrings) {
     return i === arr.length - 1 ? letter : `${letter}***`;
   }).join('');
 }
-console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
-console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
+// console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+// console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
