f1d6d3ed40201e2e88e43ee9d47af3225db64479
Lewis Powers <lpowers1985@gmail.com>
2022-06-12 18:01:45 -0700

    make string upper or lower case depending on num of upper/lower case letters

---
+++ fixStringCase.js
@@ -0,0 +1,10 @@
+function solve(str) {
+  let lowerCaseCount = 0;
+  let upperCaseCount = 0;
+  for (let i in str) {
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 91) upperCaseCount++
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) lowerCaseCount++
+  }
+  return upperCaseCount > lowerCaseCount ? str.toUpperCase() : str.toLowersCase()
+}
+console.log(String.fromCharCode(65))
