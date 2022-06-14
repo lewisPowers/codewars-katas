488e09a0ab463207603c7242e2941c7554b6dae1
Lewis Powers <lpowers1985@gmail.com>
2022-06-14 02:50:00 -0700

    lp custom: validate brackets

---
+++ checkAllBrackets.js
@@ -0,0 +1,26 @@
+//  check string for correct formatting of opening and closing parentheses, brackets, carets, etc.
+function isValid(str) {
+  let brackets = {
+    opening: ['(', '[', '<', '{'],
+    closing: [')', ']', '>', '}'],
+    count: {
+      '(': 0,
+      '[': 0,
+      '<': 0,
+      '{': 0
+    }
+  };
+  for (let char of str) {
+    if (brackets.closing.includes(char)) {
+      let relativeCountKey = brackets.opening[brackets.closing.indexOf(char)];
+      brackets.count[relativeCountKey]--;
+    }
+    if (brackets.count[char] < 0) return false;
+    if (brackets.opening.includes(char)) brackets.count[char]++;
+  }
+  let isValid = true
+  Object.keys(brackets.count).forEach(symbol => {
+    if (brackets.count[symbol] !== 0) isValid = false;
+  })
+  return isValid;
+}
