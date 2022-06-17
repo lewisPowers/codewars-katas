fd56fc1a50b65af466f0f29371da98387fcf155f
Lewis Powers <lpowers1985@gmail.com>
2022-06-16 18:46:27 -0700

    convert stringified names into new format

--- meeting.js
+++ meeting.js
@@ -1,6 +1,13 @@
 function meeting(s) {
-  // split people
-// sort by last name
-// collect people by last name; if collection length is > 1, sort them by first name
-
+return s.toUpperCase().split(';').map(name => {
+    let [firstName, lastName] = name.split(':')
+    return `(${lastName}, ${firstName})`
+  }).sort().join('')
 }
+
+let names = "Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn"
+let multLastNames = "Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern;Lewis:Powers;Joan:Powers"
+// console.log(names)
+// console.log(multLastNames)
+// console.log(meeting(names))
+// console.log(meeting(multLastNames))
