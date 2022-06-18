c2f881f36bd86b3679c16b5698c312df972e9666
Lewis Powers <lpowers1985@gmail.com>
2022-06-18 13:00:03 -0700

    check phone number format

---
+++ isValidPhoneNumber.js
@@ -0,0 +1,19 @@
+import frontend from '/frontendModule.js';
+
+function isValidNumber(string) {
+  // '(123) 456-7890'
+  if (string.length !== 14) return false;
+  for (let index in string) {
+    index = Number(index);
+    if ( (index === 0 && string[index] !== '(') ||
+    (index === 4 && string[index] !== ')') ||
+    (index === 5 && string[index] !== ' ') ||
+    (index === 9 && string[index] !== '-') ) return false;
+    if (index !== 0 && index !== 4 && index !== 5 && index !== 9) {
+      if (isNaN(Number(string[index])))  return false;
+    }
+  }
+  return true;
+}
+isValidNumber.newName = 'Phone Number Format'
+frontend(isValidNumber, '(123) 456-7890')
