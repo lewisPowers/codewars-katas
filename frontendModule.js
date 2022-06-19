fd88ca1c63350a3ae182da82f8bc4d73dd677bdb
Lewis Powers <lpowers1985@gmail.com>
2022-06-19 03:20:20 -0700

    cleanup format

--- frontendModule.js
+++ frontendModule.js
@@ -1,9 +1,9 @@
 import formatCode from '/formatCode.js';
 
 export default function(fn, args) {
-  let allArgsArr = Array.from(arguments)
+  let allArgsArr = Array.from(arguments);
   allArgsArr.shift();
-  let call = fn.apply(this, allArgsArr)
+  let call = fn.apply(this, allArgsArr);
   let box = document.createElement('div');
   box.style.maxWidth = '80%';
   box.style.margin = '20px';
@@ -11,7 +11,7 @@ export default function(fn, args) {
   box.style.border = '1px solid gray';
   let heading = document.createElement('h2');
   let content = document.createElement('h4');
-  let code = formatCode(fn.toString())
+  let code = formatCode(fn.toString());
   let answer = document.createElement('h3');
   heading.textContent = `Name: ${fn.newName || fn.name}`;
   content.textContent = `Input: ${allArgsArr}`;
