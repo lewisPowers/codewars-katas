016e0be44f9906e043a0813b3ca632398f43830b
Lewis Powers <lpowers1985@gmail.com>
2022-06-19 01:24:34 -0700

    format, cleanup

--- fixStringCase.js
+++ fixStringCase.js
@@ -1,10 +1,13 @@
+import frontend from '/frontendModule.js';
+
 function solve(str) {
   let lowerCaseCount = 0;
   let upperCaseCount = 0;
   for (let i in str) {
-    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 91) upperCaseCount++
-    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) lowerCaseCount++
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 91) upperCaseCount++;
+    if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) lowerCaseCount++;
   }
-  return upperCaseCount > lowerCaseCount ? str.toUpperCase() : str.toLowersCase()
+  return upperCaseCount > lowerCaseCount ? str.toUpperCase() : str.toLowerCase();
 }
-// console.log(String.fromCharCode(65))
+solve.newName = 'Fix String Case';
+frontend(solve, 'CoDe');
