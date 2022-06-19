98aa0ea92873343f226f110b7d2983fbc5c7f62c
Lewis Powers <lpowers1985@gmail.com>
2022-06-19 03:21:27 -0700

    test 'branch'

--- formatCode.js
+++ formatCode.js
@@ -3,7 +3,7 @@ export default function formatCode(fnString) {
   let pElement = createElement('p');
   let line = '';
   let leftMargin = 0;
-  let triggers = [';', '{', '}', '`'];
+  let triggers = [';', '{', '}', '`', ','];
   for (let i = 0; i < fnString.length; i++) {
     let char = fnString[i];
     if (triggers.includes(fnString[i])) {
@@ -16,10 +16,15 @@ export default function formatCode(fnString) {
         line += fnString[i];
       } else if (!formatCode['`'] || formatCode['`'] % 2 === 0) {
         if ( fnString[i] === '}' && (fnString[i + 1] === ')' ||
-        fnString[i + 1] === ';' || fnString[i + 1] === "'") ) {
+        fnString[i + 1] === ';' || fnString[i + 1] === "'") ||
+        (fnString[i] === ';' && fnString[i + 1] === "'") ||
+        (fnString[i] === ',' && fnString[i - 1] !== "]") ||
+        (fnString[i] === '{' && fnString[i + 1] === "'")) {
           line += fnString[i];
+          if ( fnString[i] === '}' && fnString[i + 1] === ')') leftMargin -= 11
         } else {
-          pElement = newLine(fnString[i], line);
+
+          pElement = newLine(fnString[i], line, i);
           line = '';
         }
       } else {
@@ -31,24 +36,35 @@ export default function formatCode(fnString) {
   }
   return mainContainer;
 
-  function newLine(char, substr) {
+  function newLine(char, substr, charPosition, margin) {
     let newPElement;
-    if (char === ';') {
+    if (char === ';' || char === ',') {
       substr += char;
       pElement.textContent = substr;
       mainContainer.append(pElement);
+      // if (char === ';' && fnString.slice(charPosition, charPosition + 5).includes('}')) {
+      //   leftMargin -= 11;
+      // }
       newPElement = createElement('p', leftMargin)
     } else if (char === '{') {
       substr += char;
       pElement.textContent = substr;
       mainContainer.append(pElement);
-      leftMargin += 11
+      leftMargin += 11;
       newPElement = createElement('p', leftMargin)
     } else if (char === '}') {
       if (substr.length > 1 || substr !== ' ') {
         pElement.textContent = substr;
         mainContainer.append(pElement);
       }
+      // if (fnString.slice(charPosition - 3, charPosition - 1).includes('}')) {
+      //   console.log('FOUND A } NEARBY')
+      //   leftMargin -= 11;
+      // }
+      // let slice = fnString.slice(charPosition - 6, charPosition)
+      // console.log(char, slice)
+      // if (slice.includes('}')) leftMargin -= 11;
+      // if (fnString.includes('diamond') && fnString[charPosition + 1] === ')') leftMargin = leftMargin - 11
       leftMargin -= 11;
       newPElement = createElement('p', leftMargin);
       newPElement.textContent = '}';
