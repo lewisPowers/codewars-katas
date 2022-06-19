e048e2eb1e0bde98a57c97191f28037bd74859b0
Lewis Powers <lpowers1985@gmail.com>
2022-06-18 22:24:27 -0700

    format code into new lines: BETA

--- formatCode.js
+++ formatCode.js
@@ -1,49 +1,105 @@
-export default function formatCode(fnString, targetElement, margin = 0) {
-  if (arguments.length > 2) {
-    formatCode['resultDiv'] = createElement('div')
-    formatCode['targetElement'] = createElement('p', margin);
-    targetElement = formatCode['targetElement'];
+export default function formatCode(fnString) {
+  let mainContainer = createElement('div');
+  let pElement = createElement('p');
+  let line = '';
+  let leftMargin = 0;
+  let triggers = [';', '{', '}'];
+  for (let i in fnString) {
+    console.log(fnString[Number(i) + 1] === ' ')
+    console.log(fnString.charCodeAt(Number(i) + 1) === 32)
+    if (triggers.includes(fnString[i])) {
+      pElement = newLine(fnString[i], line);
+      line = '';
+    } else {
+      line += fnString[i];
+    }
   }
-  if (fnString.length === 0) {
-    // append all to div, then
-    formatCode['resultDiv'].append(targetElement)
-    document.body.append(formatCode['resultDiv'])
+  return mainContainer;
+
+  function newLine(char, substr) {
+    let newPElement;
+    if (char === ';') {
+      substr += char;
+      pElement.textContent = substr;
+      mainContainer.append(pElement);
+      newPElement = createElement('p', leftMargin)
+    } else if (char === '{') {
+      substr += char;
+      pElement.textContent = substr;
+      mainContainer.append(pElement);
+      leftMargin += 11
+      newPElement = createElement('p', leftMargin)
+    } else if (char === '}') {
+      if (substr.length > 1 || substr !== ' ') {
+        pElement.textContent = substr;
+        mainContainer.append(pElement);
+      }
+      leftMargin -= 11;
+      newPElement = createElement('p', leftMargin);
+      newPElement.textContent = '}';
+      mainContainer.append(newPElement);
+      newPElement = createElement('p', leftMargin)
+    }
+    return newPElement;
   }
-  if (fnString[0] === ';') {
-    // create p element, start to append to it
-    targetElement.textContent += fnString[0];
-    formatCode['resultDiv'].append(targetElement)
-    fnString = fnString.slice(1)
-    let $pEl = createElement('p', margin);
-    return formatCode(fnString, $pEl, margin)
-  }
-  if (fnString[0] === '{') {
-    // create p element, add margin, start to append to it
-    targetElement.textContent += fnString[0];
-    formatCode['resultDiv'].append(targetElement)
-    fnString = fnString.slice(1)
-    let $pEl = createElement('p', margin);
-    return formatCode(fnString, $pEl, margin + 10)
-  }
-  if (fnString[0] === '}') {
-    // create p element, add margin, start to append to it
-    targetElement.textContent += fnString[0];
-    formatCode['resultDiv'].append(targetElement)
-    fnString = fnString.slice(1)
-    let $pEl = createElement('p', margin);
-    return formatCode(fnString, $pEl, margin - 10)
-  }
-  targetElement.textContent += fnString[0];
-  fnString = fnString.slice(1)
-  return formatCode(fnString, targetElement, margin)
 }
+
+
 
 function createElement(tag, leftMargin) {
   let $el = document.createElement(tag);
+  if (tag === 'p') {
+    $el.style.margin = '0';
+    $el.style.padding = '0';
+    $el.style.fontFamily = 'monospace';
+    $el.style.backgroundColor = `#dededede`;
+    $el.style.maxWidth = 'max-content';
+  }
   if (arguments.length > 1) $el.style.marginLeft = `${leftMargin}px`;
   return $el;
 }
 
 
+
 formatCode("function array(arr){ let split = arr.split(','); split.shift(); split.pop(); return split.join(' ') === '' ? null : split.join(' '); }")
-console.log()
+
+// export default function formatCode(fnString, targetElement, margin = 0) {
+//   if (arguments.length > 2) {
+//     formatCode['resultDiv'] = createElement('div');
+//     formatCode['targetElement'] = createElement('p', margin);
+//     targetElement = formatCode['targetElement'];
+//   }
+//   if (fnString.length === 0) {
+//     // append all to div, then div to body
+//     formatCode['resultDiv'].append(targetElement);
+//     document.body.append(formatCode['resultDiv']);
+//   }
+//   if (fnString[0] === ';') {
+//     // create p element, start to append to it
+//     targetElement.textContent += fnString[0];
+//     formatCode['resultDiv'].append(targetElement);
+//     fnString = fnString.slice(1);
+//     let $pEl = createElement('p', margin);
+//     return formatCode(fnString, $pEl, margin);
+//   }
+//   if (fnString[0] === '{') {
+//     // create p element, add margin, start to append to it
+//     targetElement.textContent += fnString[0];
+//     formatCode['resultDiv'].append(targetElement);
+//     fnString = fnString.slice(1);
+//     let $pEl = createElement('p', margin);
+//     return formatCode(fnString, $pEl, margin + 10);
+//   }
+//   if (fnString[0] === '}') {
+//     // create p element, add margin, start to append to it
+//     targetElement.textContent += fnString[0];
+//     formatCode['resultDiv'].append(targetElement);
+//     fnString = fnString.slice(1);
+//     let $pEl = createElement('p', margin);
+//     return formatCode(fnString, $pEl, margin - 10);
+//   }
+//   targetElement.textContent += fnString[0];
+//   fnString = fnString.slice(1);
+//   return formatCode(fnString, targetElement, margin);
+// }
+
--- frontendModule.js
+++ frontendModule.js
@@ -1,7 +1,8 @@
+import formatCode from '/formatCode.js';
+
 export default function(fn, args) {
   let allArgsArr = Array.from(arguments)
   allArgsArr.shift();
-  console.log(allArgsArr)
   let call = fn.apply(this, allArgsArr)
   let box = document.createElement('div');
   box.style.maxWidth = '80%';
@@ -11,57 +12,14 @@ export default function(fn, args) {
 
   let heading = document.createElement('h2');
   let content = document.createElement('h4');
-  let code = document.createElement('code');
-  // formatCode(fn.toString(), code)
+  // let code = document.createElement('code');
+  let code = formatCode(fn.toString())
   let answer = document.createElement('h3');
   heading.textContent = `Name: ${fn.newName || fn.name}`;
-  // code.textContent = `${formatCode(fn.toString())}`;
-  code.textContent = `${fn}`;
-  code.style.backgroundColor = 'lightgray'
+  // code.textContent = `${fn}`;
+  // code.style.backgroundColor = 'lightgray'
   content.textContent = `Input: ${allArgsArr}`;
-  // content.style.maxWidth = 'min-content';
   answer.textContent = `Output: ${call}`
   box.append(heading, code, content, answer)
   document.body.append(box);
 }
-
-// function formatCode(fnString, targetElement, margin = 0) {
-//   if (arguments.length < 2) {
-//     formatCode['resultDiv'] = createElement('div')
-//     formatCode['target'] = createElement('p', margin);
-//     targetElement = formatCode['target'];
-//   }
-//   if (fnString.length === 0) {
-//     // append all to div, then
-//     formatCode['resultDiv'].append(targetElement)
-//     document.body.append(formatCode['resultDiv'])
-//   }
-//   if (fnString[0] === ';') {
-//     // create p element, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement)
-//     fnString = fnString.slice(1)
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin)
-//   }
-//   if (fnString[0] === '{') {
-//     // create p element, add margin, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement)
-//     fnString = fnString.slice(1)
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin + 10)
-//   }
-//   if (fnString[0] === '}') {
-//     // create p element, add margin, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement)
-//     fnString = fnString.slice(1)
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin - 10)
-//   }
-//   targetElement.textContent += fnString[0];
-//   fnString = fnString.slice(1)
-//   return formatCode(fnString, targetElement, margin)
-// }
-
