e0cc1aea9956dc6132feca15a0af590581f04be5
Lewis Powers <lpowers1985@gmail.com>
2022-06-19 01:26:22 -0700

    cleanup

--- formatCode.js
+++ formatCode.js
@@ -59,8 +59,6 @@ export default function formatCode(fnString) {
   }
 }
 
-
-
 function createElement(tag, leftMargin) {
   let $el = document.createElement(tag);
   if (tag === 'p') {
@@ -73,48 +71,4 @@ function createElement(tag, leftMargin) {
   if (arguments.length > 1) $el.style.marginLeft = `${leftMargin}px`;
   return $el;
 }
-
-
-
-formatCode("function array(arr){ let split = arr.split(','); split.shift(); split.pop(); return split.join(' ') === '' ? null : split.join(' '); }")
-
-// export default function formatCode(fnString, targetElement, margin = 0) {
-//   if (arguments.length > 2) {
-//     formatCode['resultDiv'] = createElement('div');
-//     formatCode['targetElement'] = createElement('p', margin);
-//     targetElement = formatCode['targetElement'];
-//   }
-//   if (fnString.length === 0) {
-//     // append all to div, then div to body
-//     formatCode['resultDiv'].append(targetElement);
-//     document.body.append(formatCode['resultDiv']);
-//   }
-//   if (fnString[0] === ';') {
-//     // create p element, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement);
-//     fnString = fnString.slice(1);
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin);
-//   }
-//   if (fnString[0] === '{') {
-//     // create p element, add margin, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement);
-//     fnString = fnString.slice(1);
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin + 10);
-//   }
-//   if (fnString[0] === '}') {
-//     // create p element, add margin, start to append to it
-//     targetElement.textContent += fnString[0];
-//     formatCode['resultDiv'].append(targetElement);
-//     fnString = fnString.slice(1);
-//     let $pEl = createElement('p', margin);
-//     return formatCode(fnString, $pEl, margin - 10);
-//   }
-//   targetElement.textContent += fnString[0];
-//   fnString = fnString.slice(1);
-//   return formatCode(fnString, targetElement, margin);
-// }
 
