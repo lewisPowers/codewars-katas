6837c730d887ffe056dbb5a9ed4c19b9b91fe842
Lewis Powers <lpowers1985@gmail.com>
2022-06-21 00:29:12 -0700

    buildEl, onSubmit & other refactors

--- testCodeModule.js
+++ testCodeModule.js
@@ -1,56 +1,58 @@
-export default function(fn, argsArr) {
-  // create input for arguments
-  // on submit, call function
-  // display output on screen
+export default function(fn, argsArr, targetId) {
   let container = document.createElement('div');
   container.style.display = 'flex';
   container.style.maxHeight = '1.1rem';
   let title = document.createElement('h3');
   title.textContent = fn.newName || fn.name;
   argsArr.forEach( (arg, i) => {
-    // console.log (argsArr)
-    // debugger;
-    // let inlineArg = document.createElement('h4');
-    // inlineArg.textContent = arg
-    // container.append(inlineArg)
-    let input = document.createElement('input');
-    input.type = 'text';
-    input.classList.add('argument');
-    input.value = arg;
-    input.style.maxWidth = '2.2rem';
-    input.style.margin = '0 .15rem';
-    container.append(input);
-    if ( i !== argsArr.length - 1) container.append(makeCommas('h3'))
-  })
-  let closingArgs = document.createElement('h4');
-  closingArgs.style.margin = '0 .15rem';
-  closingArgs.textContent = ')'
+    container.append(buildInputEl(fn, arg));
+    if ( i !== argsArr.length - 1) container.append(makeCommas('h3'));
+  });
+  let closing = document.createElement('h4');
+  closing.style.margin = '0 0.15rem';
+  closing.style.padding = '0';
+  closing.textContent = ')';
   let submit = document.createElement('input');
   submit.type = 'submit';
-  container.append(closingArgs, submit)
-  submit.addEventListener('click', function(e) {
+  submit.style.verticalAlign = 'top';
+  submit.id = targetId;
+  submit.value = 'Run Test';
+
+  container.append(closing, submit);
+
+  let onSubmit = function(e) {
     let args = [];
-    Array.from(document.getElementsByClassName('argument')).forEach(arg => {
+    let moduleClicked = e.target.closest('section');
+    Array.from(moduleClicked.getElementsByClassName('argument')).forEach(arg => {
       args.push(arg.value)
-    })
-    this['answer'] = fn.apply(this, args);
-    // find target based on click
-    let moduleClicked = this.closest('section')
-    console.log(moduleClicked)
+    });
+    let key = fn.toString();
+    fn[key] = fn.apply(this, args);
     let children = moduleClicked.children;
-    console.log(children)
-    for (let i = 0; i < children.length; i++) {
-      let child = children[i]
-      if (child.className === 'answer') {
-        child.textContent = this.answer;
-      }
-    }
-  })
+    let span = children[children.length - 1];
+    span.textContent = `Test Output: ${fn[key]}`;
+  }
+
+  submit.addEventListener('click', onSubmit);
+
   return container;
+
   function makeCommas(elType) {
     let el = document.createElement(elType);
     el.style.margin = '0';
     el.textContent = ',';
     return el;
+  }
+
+  function buildInputEl(fn, arg) {
+    let input = document.createElement('input');
+    input.classList.add('argument');
+    input.value = arg;
+    input.style.width = `${fn.inputWidth}ch`;
+    input.style.margin = '0 .15rem';
+    input.style.padding = '0 .15rem';
+    input.style.textAlign = 'center';
+    input.style.transform = 'translateY(5%)';
+    return input;
   }
 }
