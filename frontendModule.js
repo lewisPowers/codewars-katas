f9a8b575d628a17a4bc3e11152e8c26e94b08e94
Lewis Powers <lpowers1985@gmail.com>
2022-06-21 00:28:02 -0700

    createEl fn & other refactors

--- frontendModule.js
+++ frontendModule.js
@@ -2,38 +2,92 @@ import formatCode from '/formatCode.js';
 import testCode from '/testCodeModule.js';
 
 export default function(fn, args) {
-  let allArgsArr = Array.from(arguments);
-  allArgsArr.shift();
+  let allArgsArr = getArgArr(arguments);
   let call = fn.apply(this, allArgsArr);
-  let box = document.createElement('section');
-  box.classList.add('module');
-  box.style.maxWidth = '80%';
-  box.style.margin = '20px';
-  box.style.padding = '20px';
-  box.style.border = '1px solid gray';
-  let heading = document.createElement('h2');
-  let content = document.createElement('h4');
+
+  let box = createEl('box', fn);
+  let heading = createEl('h2');
+  let exampleDiv = createEl('div', 'gray');
+
+  let testDiv = createEl('div', 'green');
+  let exampleInput = createEl('h4');
+  let exampleOutput = createEl('h4', box.id);
+  let testInput = createEl('h4', 'test-input');
+  let testOutput = createEl('span', ['answer', 'green']);
   let code = formatCode(fn.toString());
-  let answer = document.createElement('h3');
-  let span = document.createElement('span')
-  span.classList.add('answer')
-  let test = document.createElement('div')
+
   heading.textContent = `Name: ${fn.newName || fn.name}`;
-  // content.textContent = `Input: ${allArgsArr}`;
-  content.textContent = `Input: ${fn.name}(`;
-
-  content.style.display = 'flex';
-  // content.style.alignItems = 'flex-start';
-  content.append(testCode(fn, allArgsArr));
-
-  answer.textContent = `Output: ${call}`;
-
-  // answer.textContent = `Output: `;
-  // span.textContent = `${fn.answer || call}`;
-
-  // answer.append(span)
-  // box.append(heading, code, content, answer, testCode(fn, args));
-
-  box.append(heading, code, content, answer, span);
+  exampleInput.textContent = `Input: ${allArgsArr}`;
+  exampleOutput.textContent = `Output: ${call}`;
+  testInput.textContent = `Test Input: ${fn.name}(`;
+  testOutput.textContent = `Test Output: `;
+  exampleDiv.append(exampleInput, exampleOutput);
+  testDiv.append(testInput);
+  testInput.append(testCode(fn, allArgsArr, box.id));
+  box.append(heading, code, exampleDiv, testDiv, testOutput);
   document.body.append(box);
 }
+
+function uuid() {
+  let id = '';
+  for (let i = 0; i < 32; i++) {
+    if (i === 8 || i === 12 || i === 16 || i === 20) id += '-';
+    let randomInt = Math.floor(Math.random() * 16);
+    id += randomInt.toString(16);
+  }
+  return id;
+}
+
+function getArgArr(arrLike) {
+  let allArgsArr = Array.from(arrLike);
+  allArgsArr.shift();
+  return allArgsArr;
+}
+
+function createEl(tagOrClass, classesOrXtra) {
+  if (tagOrClass === 'box') return buildBox(classesOrXtra);
+  let $el = document.createElement(tagOrClass);
+  if (classesOrXtra && Array.isArray(classesOrXtra)) {
+    classesOrXtra.forEach(className => {
+      $el.classList.add(className)
+    })
+  } else if (classesOrXtra) $el.className = classesOrXtra;
+  if ($el.className === 'test-input') $el.style.display = 'flex';
+  if (tagOrClass === 'div' || $el.className.includes('green') || $el.className.includes('test-input')) {
+    $el.style.margin = '0';
+    $el.style.padding = '0';
+    if ($el.className.includes('gray')) {
+      $el.style.marginTop = '5px';
+      $el.style.borderTopLeftRadius = '6px';
+      $el.style.borderTopRightRadius = '6px';
+      $el.style.padding = '5px 10px';
+      $el.style.backgroundColor = 'rgba(5,5,255,.25)';
+      $el.style.height = '100%'
+    }
+    if ($el.className.includes('green')) {
+      $el.style.padding = '10px';
+      $el.style.backgroundColor = 'rgba(5,255,5,.25)';
+      if (tagOrClass === 'span') {
+        $el.style.fontWeight = '700';
+        $el.style.paddingTop = 0;
+      }
+    }
+  }
+
+  return $el;
+
+  function buildBox(fn) {
+    let box = document.createElement('section');
+    box.id = uuid();
+    box.classList.add(box.id, 'module');
+    box.fn = fn;
+    box.style.maxWidth = '80%';
+    box.style.overflow = 'hidden';
+    box.style.margin = '20px';
+    box.style.padding = '20px';
+    box.style.border = '1px solid gray';
+    return box;
+  }
+}
+
+
