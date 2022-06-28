0ca29f570515843864eb457a49690e407f42c245
Lewis Powers <lpowers1985@gmail.com>
2022-06-28 16:35:44 -0700

    cleanup: comments, semi-colons

--- modules/funcInfo.js
+++ modules/funcInfo.js
@@ -1,19 +1,24 @@
-export default function(targetElement, funcInfo) {
-  // build info tab element; add to box
-  // build hidden div with info about function
-  // when clicked, expose info div for user
-  // when clicked outide of or esc pressed, hide div
+export default function(targetElement, funcInfo, linkStr) {
   let container = document.createElement('div');
   let infoBtn = document.createElement('div');
   let infoTab = document.createElement('div');
+
+  let pEl =  document.createElement('p');
+  let linkEl = document.createElement('a');
+  linkEl.href = linkStr || 'https://google.com';
+  linkEl.target = '_blank';
+  linkEl.textContent = 'Try it yourself';
+
   infoBtn.classList.add('info-btn');
   infoTab.classList.add('info-tab', 'hidden');
-  infoTab.textContent = funcInfo || 'TEST TEST 1...2...3...4.5.6';
+  pEl.textContent = funcInfo || 'TEST TEST 1...2...3...4.5.6';
+  infoTab.append(pEl, linkEl);
 
   infoBtn.addEventListener('click', function(e) {
-    let moduleClicked = e.target.closest('section')
+    let moduleClicked = e.target.closest('section');
     let tab = moduleClicked.getElementsByClassName('info-tab')[0];
-    tab.classList.toggle('hidden')
+    moduleClicked.classList.toggle('darken');
+    tab.classList.toggle('hidden');
   })
 
   container.append(infoTab, infoBtn);
