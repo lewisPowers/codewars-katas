7e4382bfe04ffd85fdb0c7675a5b741843627b57
Lewis Powers <lpowers1985@gmail.com>
2022-07-01 11:35:22 -0700

    formats inputs for frontend visual

---
+++ modules/formatStrings.js
@@ -0,0 +1,45 @@
+export default function(input) {
+
+  return formatOutputString(input)
+
+
+  function formatOutputString(input) {
+    if (Array.isArray(input)) {
+      // console.log('in ARRAY statement')
+      return formatArrayIntoString(input)
+    } else if (Object.prototype.isPrototypeOf(input)) {
+      // console.log('in OBJECT statement')
+      return formatObjectIntoString(input);
+    } else if (typeof input === 'string') {
+      return `"${input}"`;
+    } else {
+      return input;
+    }
+  }
+
+  function formatObjectIntoString(obj) {
+    let objStr = '{';
+      Object.keys(obj).forEach( (key, i) => {
+          if (i === Object.keys(obj).length - 1) {
+              objStr += `"${key}": ${formatOutputString(obj[key])}`;
+          } else {
+              objStr += `"${key}": ${formatOutputString(obj[key])}, `;
+          }
+      })
+      objStr += '}';
+      return objStr;
+  }
+
+  function formatArrayIntoString(arr) {
+    let arrStr = '[ ';
+      arr.forEach( (el, i) => {
+          if (i === arr.length - 1) {
+            arrStr += `${formatOutputString(el)}`;
+          } else {
+            arrStr += `${formatOutputString(el)}, `;
+          }
+      })
+      arrStr += ' ]';
+      return arrStr;
+  }
+}
