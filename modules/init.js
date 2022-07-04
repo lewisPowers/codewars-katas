12b11ce9f1bdf9130e241221434341c4a1c9623c
Lewis Powers <lpowers1985@gmail.com>
2022-07-04 09:48:22 -0700

    setTimeout added to hide empty divs in html

--- modules/init.js
+++ modules/init.js
@@ -3,3 +3,8 @@ import listeners from '/modules/bodyListeners.js';
 
 allScripts();
 listeners();
+setTimeout(function() {
+  Array.from(document.getElementsByClassName('rank-div')).forEach(div => {
+    if (div.children.length < 2) div.style.display = 'none';
+  })
+}, 100);
