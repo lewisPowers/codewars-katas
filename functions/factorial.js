823e3786715679604417b145c44d632e8694b796
Lewis Powers <lpowers1985@gmail.com>
2022-07-06 00:00:35 -0700

    name change

--- functions/factorial.js
+++ functions/factorial.js
@@ -1,28 +1,14 @@
-import frontend from '../modules/frontendModule.js';
+import frontend from '/modules/frontendModule.js';
 
-// ONE LINE ITERATIVE OPTION
-function factorial(n){
-  return n < 1 || isNaN(n) || parseInt(n) !== n ? false : new Array(n).fill(0).map((num, i) => i + 1 ).reduce((a,c) => a + c );
-};
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
 
-factorial.newName = "Factorial of N";
-factorial.inputWidth = 5;
-factorial.info = 'Factorial addition style. Enter a number and the function will add all integers from 1 to n.';
-factorial.link = '';
-frontend(factorial, 100);
+factorial.newName = 'Recursion #1 - Factorial';
+factorial.link = 'https://www.codewars.com/kata/5694cb0ec554589633000036';
+factorial.kyu = 7;
+factorial.inputWidth = 4;
+factorial.info = `Factorial function that receives n and returns n!, using recursion.`;
 
-//.  ITERATIVE OPTION A:
-// function f(n){
-//   if (isNaN(n) || n < 1 || parseInt(n) !== n) return false;
-//   let arr = [];
-//   while (n > 0) {
-//     arr.push(n);
-//     n--;
-//   }
-//   return arr.reduce((a,c) => a + c, 0)
-// };
-
-  //   RECURSIVE OPTION FIRST: CALL STACK EXCEEDED FOR LARGE NUMBERS
-// function f(n){
-//   return n < 1 || isNaN(n) || parseInt(n) !== n ? false : n === 1 ? n : n + f(n - 1);
-// };
+frontend(factorial, 3); // == 6
--- functions/removeExclamations.js
+++ functions/removeExclamations.js
@@ -1,21 +1,20 @@
 import frontend from '/modules/frontendModule.js';
 
-function remove(str,n){
-  let arr = str.split('');
-  let times = n;
-  return arr.filter(char => {
-    if (char !== '!') return char;
-    if (char === '!' && times > 0) {
-      times--;
-    } else if (char === '!' && times < 1) return char;
-  }).join('')
+function remove (str) {
+  while (str[str.length - 1] === '!') str = str.slice(0, str.length - 1);
+  return str;
 }
 
-remove.newName = 'Remove Extra Marks from the End';
-remove.inputWidth = 9;
+remove.newName = 'Exclamation marks series #2';
+remove.link = 'https://www.codewars.com/kata/57faece99610ced690000165';
+remove.inputWidth = 7
 remove.kyu = 8;
-remove.info = 'Remove n exclamation marks in the sentence from left to right.';
-remove.link = 'https://www.codewars.com/kata/57faf7275c991027af000679';
+remove.info = `Removes all exclamation marks from the end of sentence.`;
+frontend(remove, 'Hi! Hi!! Hi!!!!')
 
-frontend(remove, 'Wow!!!!', 2)
-
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
