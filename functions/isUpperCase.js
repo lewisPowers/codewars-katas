ecc73693c7efe078bdb00ce654f700d7c3f18f6a
Lewis Powers <lpowers1985@gmail.com>
2022-07-07 03:08:45 -0700

    missing semicolons; cleanup

--- functions/isUpperCase.js
+++ functions/isUpperCase.js
@@ -6,17 +6,11 @@
 //   return true;
 // }
 
-// String.prototype.isUpperCase.newName = 'Is the String Uppercase?'
-// String.prototype.isUpperCase.link = 'https://www.codewars.com/kata/56cd44e1aa4ac7879200010b';
-// String.prototype.isUpperCase.kyu = 8;
-// String.prototype.isUpperCase.inputWidth = 15;
-// String.prototype.isUpperCase.info = `Function that checks whether the string is ALL CAPS`;
-
 import frontend from '/modules/frontendModule.js';
 
 function isUpperCase(string) {
   for (let char of string) {
-    let code = char.charCodeAt(0)
+    let code = char.charCodeAt(0);
     if ( code >= 97 && code <= 122 ) return false;
   }
   return true;
