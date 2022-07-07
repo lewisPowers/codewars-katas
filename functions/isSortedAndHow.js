e7ee9f36443358174eb5e282ce2fca2a25a73b6c
Lewis Powers <lpowers1985@gmail.com>
2022-07-07 03:08:34 -0700

    missing semicolons; cleanup

--- functions/isSortedAndHow.js
+++ functions/isSortedAndHow.js
@@ -1,17 +1,17 @@
 import frontend from '/modules/frontendModule.js';
 
 function isSortedAndHow(array) {
-  let copy = array.slice()
+  let copy = array.slice();
   let sorted = function(isAsc) {
     return copy.sort((a, b) => {
       if (isAsc) return a - b;
       return b - a;
-    })
-  }
+    });
+  };
   let isSame = function(sortedArr) {
     for (let i = 0; i < array.length; i++) if (array[i] !== sortedArr[i]) return false;
     return true;
-  }
+  };
   for (let i = 0; i < array.length; i++) {
     if (isSame(sorted(true))) return 'yes, ascending';
     if (isSame(sorted())) return 'yes, descending';
