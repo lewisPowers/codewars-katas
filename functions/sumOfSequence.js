3f29ff054538ab73ca605bbe112d84e09aa01a3b
Lewis Powers <lpowers1985@gmail.com>
2022-07-07 03:10:26 -0700

    missing semicolons; cleanup

--- functions/sumOfSequence.js
+++ functions/sumOfSequence.js
@@ -4,7 +4,7 @@ function sequenceSum(begin, end, step) {
   let sum = 0;
   for (let i = begin; i <= end; i += step) sum += i;
   return sum;
-};
+}
 
 
 sequenceSum.link = 'https://www.codewars.com/kata/586f6741c66d18c22800010a';
