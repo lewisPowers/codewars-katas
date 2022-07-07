9d85638d06a93a1b6bdb5ff7f89c31c8615b840c
Lewis Powers <lpowers1985@gmail.com>
2022-07-07 03:08:12 -0700

    missing semicolons; cleanup

--- functions/hexToDec.js
+++ functions/hexToDec.js
@@ -1,13 +1,7 @@
 import frontend from '/modules/frontendModule.js';
 
 function hexToDec(hexString){
-  // EXAMPLE
-  //   assert.strictEqual(hexToDec("1"), 1);
-  // assert.strictEqual(hexToDec("a"), 10);
-  // assert.strictEqual(hexToDec("10"), 16);
-  // assert.strictEqual(hexToDec("FF"), 255);
-  // assert.strictEqual(hexToDec("-C"), -12);
-  return parseInt(hexString, 16)
+  return parseInt(hexString, 16);
 }
 
 hexToDec.newName = 'Hex to Decimal'
@@ -16,3 +10,10 @@ hexToDec.link = 'https://www.codewars.com/kata/57a4d500e298a7952100035d';
 hexToDec.kyu = 8;
 hexToDec.info = `This function converts hex number (given as a string) to a decimal number.`
 frontend(hexToDec, '10');
+
+// EXAMPLE
+  //   assert.strictEqual(hexToDec("1"), 1);
+  // assert.strictEqual(hexToDec("a"), 10);
+  // assert.strictEqual(hexToDec("10"), 16);
+  // assert.strictEqual(hexToDec("FF"), 255);
+  // assert.strictEqual(hexToDec("-C"), -12);
