ec25fba7335d95922c5948ae83b38375136db081
Lewis Powers <lpowers1985@gmail.com>
2022-07-12 00:06:42 -0700

    styles moved to styles.css

--- modules/testCodeModule.js
+++ modules/testCodeModule.js
@@ -1,6 +1,6 @@
 import formatStrings from '/modules/formatStrings.js';
 
-export default function(fn, argsArr, targetId, fnCall) {
+export default function(fn, argsArr, targetId) {
   let container = createEl('div', 'container-div');
   let title = createEl('h4', 'title');
   let testInput = createEl('h4', 'test-input');
@@ -37,7 +37,6 @@ export default function(fn, argsArr, targetId, fnCall) {
       }
     });
     let key = fn.toString();
-    // fn[key] = fnCall;
     fn[key] = fn.apply(this, args);
     let children = moduleClicked.children;
     let span = children[children.length - 1];
@@ -46,9 +45,8 @@ export default function(fn, argsArr, targetId, fnCall) {
     if (fn.log) {
       if (!fn.logged) {
         fn.logged = true;
-        let consoleMessageEl = createEl('p');
+        let consoleMessageEl = createEl('p', 'console-message');
         consoleMessageEl.textContent = '❗Open console to see better output❗';
-        consoleMessageEl.style.textAlign = 'center';
         moduleClicked.append(consoleMessageEl);
       }
       console.log(fn[key]);
@@ -74,178 +72,24 @@ export default function(fn, argsArr, targetId, fnCall) {
 }
 
 function makeCommas(elType) {
-  let el = document.createElement(elType);
-  el.style.margin = '0';
+  let el = createEl(elType, 'test-arg-comma');
   el.textContent = ',';
   return el;
 }
 
 function createEl(tag, type, target) {
   let $el = document.createElement(tag);
-  if (tag === 'h4') $el.style.margin = '0';
   if (type && Array.isArray(type)) {
     type.forEach(className => {
       $el.classList.add(className);
     })
   } else if (type) $el.className = type;
-  if (type === 'container-div') {
-    $el.style.display = 'flex';
-    $el.style.justifyContent = 'flex-start';
-    $el.style.maxHeight = '1.5rem';
-  } else if (type === 'submit') {
+  if (type === 'submit') {
     $el.type = 'submit';
-    $el.style.backgroundColor = 'rgba(5,255,5,.06)';
-    $el.style.borderRadius = '4px';
-    $el.style.fontFamily = 'Kdam Thmor Pro';
     $el.id = target;
     $el.value = 'Run Code';
-  } else if (type === 'closing' || type === 'opening' || type === 'title') {
-    $el.style.margin = '4px 0.15rem 0';
-    $el.style.padding = '0';
-    $el.style.fontFamily = 'monospace';
-    $el.style.fontSize = '13px';
+  } else if (type === 'closing' || type === 'opening') {
     $el.textContent = type === 'closing' ? ')' : '(';
-    if (type === 'title') {
-      $el.style.marginRight = '0';
-      $el.style.marginLeft = '10px';
-    }
-    if (type === 'opening') {
-      $el.style.marginLeft = '0';
-    }
-    if (type === 'closing' || type === 'opening') {
-      $el.style.fontSize = '19px';
-      $el.style.marginTop = '0px';
-    }
   }
   return $el;
 }
-
-
-
-
-// import formatStrings from '/modules/formatStrings.js';
-
-// export default function(fn, argsArr, targetId, fnCall) {
-//   let container = createEl('div', 'container');
-//   let title = createEl('h4', 'title');
-//   let testInput = createEl('h4', 'test-input');
-//   let opening = createEl('h4', 'opening');
-//   let closing = createEl('h4', 'closing');
-//   let submit = createEl('input', 'submit', targetId);
-
-//   title.textContent = fn.name;
-//   testInput.textContent = `Test Input: `;
-
-//   container.append(testInput, title, opening);
-
-//   argsArr.forEach( (arg, i) => {
-//     container.append(buildInputEl(fn, arg, i, container));
-//     if ( i !== argsArr.length - 1) container.append(makeCommas('h3'));
-//   });
-
-//   container.append(closing, submit);
-//   submit.addEventListener('click', onSubmit);
-//   return container;
-
-//   function onSubmit(e) {
-//     let args = [];
-//     let moduleClicked = e.target.closest('section');
-//     Array.from(moduleClicked.getElementsByClassName('argument'))
-//       .forEach(argEl => {
-//       if (argEl.value[0] === '[') {
-//         args.push(JSON.parse(argEl.value))
-//       } else if (argEl.value.includes('\\n')) {
-//         args.push(JSON.parse(argEl.value))
-//       } else if (!isNaN(Number(argEl.value.slice(1, argEl.value.length - 1))) &&
-//         (argEl.value[0] === "'" || argEl.value[0] === "\"")) {
-//         args.push(Number(argEl.value.slice(1, argEl.value.length - 1)));
-//       } else {
-//         args.push(argEl.value)
-//       }
-//     });
-//     let key = fn.toString();
-//     // fn[key] = fnCall;
-//     fn[key] = fn.apply(this, args);
-//     let children = moduleClicked.children;
-//     let span = children[children.length - 1];
-//     if (span.tagName !== 'SPAN') span = children[children.length - 2];
-//     span.textContent = `Test Output: ${formatStrings(fn[key])}`;
-//     if (fn.log) {
-//       if (!fn.logged) {
-//         fn.logged = true;
-//         let consoleMessageEl = createEl('p');
-//         consoleMessageEl.textContent = '❗Open console to see better output❗';
-//         consoleMessageEl.style.textAlign = 'center';
-//         moduleClicked.append(consoleMessageEl);
-//       }
-//       console.log(fn[key]);
-//     }
-//   }
-
-//   function buildInputEl(fn, arg, index, parent) {
-//     let input = document.createElement('input');
-//     let inputLen = arg.toString().length < 85 ? arg.toString().length : 85;
-//     input.classList.add('argument');
-//     // input.value = typeof arg === 'string' ? arg : formatStrings(arg);
-//     input.value = formatStrings(arg, true);
-//     input.style.width = `${inputLen + 2}ch`;
-//     // input.style.margin = '0 .15rem';
-//     // input.style.padding = '0 .15rem';
-//     // input.style.textAlign = 'center';
-//     // input.style.transform = 'translateY(5%)';
-//     input.addEventListener('keydown', function(event) {
-//       if (event.key === 'Enter') {
-//         event.target.dispatchEvent(onSubmit(event));
-//       } else {
-//         input.style.width = `${input.value.length + 2}ch`
-//       }
-//     })
-//     return input;
-//   }
-// }
-
-// function makeCommas(elType) {
-//   let el = document.createElement(elType);
-//   el.style.margin = '0';
-//   el.textContent = ',';
-//   return el;
-// }
-
-// function createEl(tag, type, target) {
-//   let $el = document.createElement(tag);
-//   if (tag === 'h4') $el.style.margin = '0';
-//   if (type && Array.isArray(type)) {
-//     type.forEach(className => {
-//       $el.classList.add(className);
-//     })
-//   } else if (type) $el.className = type;
-//   if (type === 'container') {
-//     $el.style.display = 'flex';
-//     $el.style.maxHeight = '1.5rem';
-//   } else if (type === 'submit') {
-//     $el.type = 'submit';
-//     $el.style.backgroundColor = 'rgba(5,255,5,.06)';
-//     $el.style.borderRadius = '4px';
-//     $el.style.fontFamily = 'Kdam Thmor Pro';
-//     $el.id = target;
-//     $el.value = 'Run Code';
-//   } else if (type === 'closing' || type === 'opening' || type === 'title') {
-//     $el.style.margin = '4px 0.15rem 0';
-//     $el.style.padding = '0';
-//     $el.style.fontFamily = 'monospace';
-//     $el.style.fontSize = '13px';
-//     $el.textContent = type === 'closing' ? ')' : '(';
-//     if (type === 'title') {
-//       $el.style.marginRight = '0';
-//       $el.style.marginLeft = '10px';
-//     }
-//     if (type === 'opening') {
-//       $el.style.marginLeft = '0';
-//     }
-//     if (type === 'closing' || type === 'opening') {
-//       $el.style.fontSize = '19px';
-//       $el.style.marginTop = '0px';
-//     }
-//   }
-//   return $el;
-// }
