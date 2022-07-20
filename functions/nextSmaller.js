9097513d6aae4ac0e7af48ad312112f535ae9629
Lewis Powers <lpowers1985@gmail.com>
2022-07-20 14:50:04 -0700

    not fully implemented

--- functions/nextSmaller.js
+++ functions/nextSmaller.js
@@ -3,54 +3,56 @@ function nextSmaller(n) {
   // if last digit is smaller than previous digit, swap and return?
   // if not, move up one and recurse
 
-  let numStr = n.toString();
-  for (let i = numStr.length - 1; i > 0; i--) {
-    let current = numStr[i];
-    let previous = numStr[i - 1];
-    if (current < previous && current !== '0') {
-      let arr = numStr.split('')
-      arr[i] = previous;
-      arr[i - 1] = current;
-      return Number(arr.join(''))
-    }
-
-  }
-  return -1;
-
-  // let baseCase = n.toString();
-  // let cases = [ baseCase ];
-
-  // if (baseCase.length < 2 || allDigitsAreSame(baseCase)) return -1;
-  // while (cases.length < 2 || cases.lastIndexOf(baseCase) === cases.indexOf(baseCase)) {
-  //   pushNPossibilities(cases[cases.length - 1]);
-  // }
-
-  // cases.sort();
-  // let nextSmallest = cases[cases.indexOf(baseCase) - 1]
-  // if (cases.length === 1 || nextSmallest === undefined || nextSmallest[0] === '0') return -1;
-  // return Number(nextSmallest);
-
-  // function pushNPossibilities(numStr) {
-  //   let arr = splitNumStrIntoArr(numStr);
-  //   for (let i = 0; i < arr.length - 1; i++) {
-  //     let swap = arr[i];
-  //     arr[i] = arr[i + 1];
-  //     arr[i + 1] = swap;
-  //     cases.push(joinNumStrings(arr))
+  // let numStr = n.toString();
+  // for (let i = numStr.length - 1; i > 0; i--) {
+  //   let current = numStr[i];
+  //   let previous = numStr[i - 1];
+  //   if (current < previous && current !== '0') {
+  //     let arr = numStr.split('')
+  //     arr[i] = previous;
+  //     arr[i - 1] = current;
+  //     return Number(arr.join(''))
   //   }
-  // }
 
-  // function splitNumStrIntoArr(numberStr) {
-  //   return numberStr.split('');
   // }
+  // return -1;
 
-  // function joinNumStrings(array) {
-  //   return array.join(''); // keep it a string!
-  // }
 
-  // function allDigitsAreSame(numStr) {
-  //   return splitNumStrIntoArr(baseCase).every((char, i, arr) => char === arr[0])
-  // }
+
+  let baseCase = n.toString();
+  let cases = [ baseCase ];
+
+  if (baseCase.length < 2 || allDigitsAreSame(baseCase)) return -1;
+  while (cases.length < 2 || cases.lastIndexOf(baseCase) === cases.indexOf(baseCase)) {
+    pushNPossibilities(cases[cases.length - 1]);
+  }
+
+  cases.sort();
+  let nextSmallest = cases[cases.indexOf(baseCase) - 1]
+  if (cases.length === 1 || nextSmallest === undefined || nextSmallest[0] === '0') return -1;
+  return Number(nextSmallest);
+
+  function pushNPossibilities(numStr) {
+    let arr = splitNumStrIntoArr(numStr);
+    for (let i = 0; i < arr.length - 1; i++) {
+      let swap = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = swap;
+      cases.push(joinNumStrings(arr))
+    }
+  }
+
+  function splitNumStrIntoArr(numberStr) {
+    return numberStr.split('');
+  }
+
+  function joinNumStrings(array) {
+    return array.join(''); // keep it a string!
+  }
+
+  function allDigitsAreSame(numStr) {
+    return splitNumStrIntoArr(baseCase).every((char, i, arr) => char === arr[0])
+  }
 
 }
 debugger;
