37dec994faf0d0f52269f85c4d6fa53cc67ed7f4
Lewis Powers <lpowers1985@gmail.com>
2022-09-01 11:58:07 -0700

    remove inputWidth prop

--- functions/compareArrays.js
+++ functions/compareArrays.js
@@ -21,7 +21,7 @@ function comp(array1, array2){
 comp.link = 'https://www.codewars.com/kata/550498447451fbbd7600041c';
 comp.kyu = 6;
 comp.newName = "Are they the 'same'?";
-comp.inputWidth = 8;
+// comp.inputWidth = 8;
 comp.info = `Checks whether two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.`
 let testArr1 = [121, 144, 19, 161, 19, 144, 19, 11];
 let testArr2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
