2b527ce2955b19f2fa75110df770cd704407524d
Lewis Powers <lpowers1985@gmail.com>
2022-09-05 16:09:22 -0700

    duplicate styles

--- modules/minimize.js
+++ modules/minimize.js
@@ -3,6 +3,7 @@ export default function() {
   btn.classList.add('minimize-btn');
   btn.addEventListener('click', function(e) {
     e.target.closest('section').classList.add('minimized');
+    e.target.closest('section').scrollIntoView( { block: 'nearest' } )
   })
   return btn;
 }
