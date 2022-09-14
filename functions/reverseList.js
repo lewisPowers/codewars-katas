e2853570d785e1526e0c20d7f069018541f57323
Lewis Powers <lpowers1985@gmail.com>
2022-09-14 15:07:14 -0700

    simple kata

---
+++ functions/reverseList.js
@@ -0,0 +1,13 @@
+import frontend from '/modules/frontendModule.js';
+
+function reverseList(list) {
+  return list.slice().reverse();
+}
+
+reverseList.newName = 'Reverse List Order';
+reverseList.kyu = 8;
+reverseList.info = 'Function that takes in a list and returns a list with the reverse order.';
+reverseList.link = 'https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b';
+
+frontend(reverseList, [1,2,3,4,5])
+/*             // ==> [5,4,3,2,1] */
