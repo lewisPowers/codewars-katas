4a20449e45c3081dcc435fbd065c1c8a4deae639
Lewis Powers <lpowers1985@gmail.com>
2022-09-15 12:17:15 -0700

    hide console ref message when minimized

--- modules/minimize.js
+++ modules/minimize.js
@@ -2,6 +2,9 @@ export default function() {
   let btn = document.createElement('div');
   btn.classList.add('minimize-btn');
   btn.addEventListener('click', function(e) {
+    if (e.target.closest('section').querySelector('.console-message') != undefined) {
+      e.target.closest('section').querySelector('.console-message').classList.add('hidden');
+    }
     e.target.closest('section').classList.add('minimized');
     e.target.closest('section').scrollIntoView( { block: 'nearest' } )
   })
